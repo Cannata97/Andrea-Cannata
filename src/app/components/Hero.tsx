3052542aeb18435c1320ea3541de0b2b0e23de9c
Andrea Cannata <andreacannata97@gmail.com>
2025-07-11 16:29:47 +0200

    init

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <section className="text-center py-20 bg-gray-100">
+      <h1 className="text-4xl text-black font-bold ">Andrea Cannata</h1>
+      <p className="text-xl text-black mt-4">Sviluppatore Web • Freelancer • Creativo</p>
+    </section>
+  );
+}
