09372a2eef5b3d78ff3d3d8755a9f334dd63f1c1
Andrea Cannata <andreacannata97@gmail.com>
2025-07-13 22:51:42 +0200

    modifiche template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,14 @@ export const metadata = {
   title: 'Andrea Cannata | Salesforce & SaaS Developer',
   description: 'I develop solutions on Salesforce, Oracle CPQ and custom SaaS web applications.',
   keywords: ['Salesforce Developer', 'Oracle CPQ', 'SaaS', 'CRM', 'Web Development', 'Freelance'],
+  verification: {
+    google: 'yT1PDpZ3Q3vh656BxXLO1M-2BlkQt1KU-y5eDzRqU0',
+  }
 };
 
 export default function Home() {
   return (
     <>
-    <meta name="google-site-verification" content="-yT1PDpZ3Q3vh656BxXLO1M-2BlkQt1KU-y5eDzRqU0" />
       <Navbar />
       <HomeClient />
     </>
